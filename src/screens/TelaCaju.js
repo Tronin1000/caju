6202798f061be17415a6c8c1fa59e2ae33b01d6e
Tronin1000 <tiagoventuratec@gmail.com>
2026-02-18 14:40:54 -0300

    projeto caju

---
+++ src/screens/TelaCaju.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import estilos from '../styles/estilosCaju';
+
+export default function TelaCaju() {
+  return (
+    <ScrollView style={estilos.container}>
+      {/* Cabeçalho */}
+      <View style={estilos.header}>
+        {/* Linha superior: Benefícios e Extrato */}
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start' }}>
+          <Text style={estilos.headerText}>Benefícios</Text>
+          <Text style={estilos.extrato}>Extrato {'>'} </Text>
+        </View>
+        
+        <Text style={estilos.saldoPequenoCardBeneficio}>Saldo disponível</Text>
+        
+        {/* Linha do R$ 12,19 com Em uso ao lado */}
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+          <Text style={estilos.saldoGrandeCardBeneficio}>R$ 12,19</Text>
+          <View style={estilos.badgeEmUso}>
+            <Text style={estilos.badgeEmUsoTexto}>Em uso</Text>
+          </View>
+        </View>
+      </View>
+      
+      {/* Card Principal de Benefícios */}
+      <View style={estilos.card}>
+        <View style={estilos.cardHeader}>
+          <Text style={estilos.cardTitle}>Benefícios</Text>
+          <TouchableOpacity>
+            <Text style={estilos.detalhes}>Detalhes {'>'} </Text>
+          </TouchableOpacity>
+        </View>
+        
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 16 }}>
+          <View>
+            <View style={estilos.categoriaCard}>
+            <Text style={estilos.categoriaTexto}>Home office</Text>
+            <Text style={estilos.categoriaValor}>R$ 0,00</Text>
+            </View>
+          </View>
+          <View>
+             <View style={estilos.categoriaCard}>
+            <Text style={estilos.categoriaTexto}>Saúde</Text>
+            <Text style={estilos.categoriaValor}>R$ 0,00</Text>
+            </View>
+          </View>
+                    <View>
+             <View style={estilos.categoriaCard}>
+            <Text style={estilos.categoriaTexto}>Mobilidade</Text>
+            <Text style={estilos.categoriaValor}>R$ 0,00</Text>
+            </View>
+          </View>
+        </View>
+        
+        <View style={estilos.divider} />
+        
+        <Text style={estilos.saldoPequeno}>Saldo disponível</Text>
+        <Text style={estilos.saldoGrandeCard}>R$ 12,19</Text>
+        
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 8 }}>
+          <Text style={estilos.fixoTexto}>Fixo: <Text style={{ fontWeight: 'bold' }}>R$ 12,19</Text></Text>
+          <Text style={estilos.flexivelTexto}>Flexível: <Text style={{ fontWeight: 'bold' }}>R$ 0,00</Text></Text>
+        </View>
+        
+        <TouchableOpacity style={estilos.botaoEnvio}>
+          <Text style={estilos.textoBotao}>
+            Envio de saldo entre categorias
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Card Saldo Caju */}
+      <View style={estilos.card}>
+        <View style={estilos.cardHeader}>
+          <View style={estilos.tituloComBadge}>
+            <Text style={estilos.cardTitle}>Saldo Caju</Text>
+            <View style={estilos.badgeNovo}>
+              <Text style={estilos.badgeTexto}>Novo</Text>
+            </View>
+          </View>
+          <TouchableOpacity>
+            <Text style={estilos.detalhes}>Detalhes {'>'} </Text>
+          </TouchableOpacity>
+        </View>
+        
+        <Text style={estilos.saldoPequeno}>Saldo disponível</Text>
+        <Text style={estilos.saldoGrandeCard}>R$ 0,39</Text>
+      </View>
+
+    </ScrollView>
+  );
+}
---
+++ src/styles/estilosCaju.js
@@ -0,0 +1,185 @@
+import { StyleSheet } from 'react-native';
+
+const estilosCaju = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#D63023',
+  },
+  header: {
+    marginHorizontal: 25,
+    marginTop: 50,
+    marginBottom: 24,
+    backgroundColor: '#af181d',
+    borderRadius: 16,
+    padding: 20,
+    height: 200,
+    overflow: 'hidden',
+  },
+
+  headerText: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    marginBottom: 30,
+  },
+  badgeEmUso: {
+    backgroundColor: '#FFDE9E',
+    borderRadius: 8,
+    paddingVertical: 3,
+    paddingHorizontal: 12,  
+  },
+  badgeEmUsoTexto: {
+    fontSize: 14,
+    color: '#AF181D',
+    fontWeight: '500',
+  },
+  saldoGrandeCardBeneficio: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#ffffff',
+  },
+  extrato: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#ffffff',
+  },
+  saldoPequenoCard: {
+    fontSize: 14,
+    color: '#111010',
+    marginBottom: 4,
+  },
+  saldoGrandeCard: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#090909',
+    marginBottom: 8,
+  },
+  saldoPequenoCardBeneficio: {
+    fontSize: 14,
+    color: '#ffffff',
+    marginBottom: 4,
+  },
+  card: {
+    backgroundColor: '#ffffff',
+    padding: 20,
+    marginBottom: 8,
+    marginHorizontal: 16,
+    borderRadius: 16,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 3,
+  },
+  cardSaldo: {
+    height: '100%',
+    backgroundColor: '#ffffff',
+    padding: 20,
+    marginBottom: 8,
+    marginHorizontal: 16,
+    borderRadius: 16,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 3,
+  },
+  cardHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#333',
+    fontWeight: 'bold'
+  },
+  detalhes: {
+    fontWeight: 'bold',
+    color: '#C21B40',
+    fontSize: 14,
+  },
+  divider: {
+    height: 1,
+    backgroundColor: '#f0f0f0',
+    marginVertical: 16,
+  },
+  flexivel: {
+    fontSize: 16,
+    color: '#333',
+    marginBottom: 16,
+  },
+  botaoEnvio: {
+    backgroundColor: '#f8f8f8',
+    padding: 10,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: '#e0e0e0',
+    marginTop: 16,
+  },
+  textoBotao: {
+    color: '#0c0c0c',
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  tituloComBadge: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  badgeNovo: {
+    backgroundColor: '#FFDE9E',
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    marginLeft: 8,
+  },
+  badgeTexto: {
+    color: '#AF181D',
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+  categoriaCard: {
+    backgroundColor: '#f1eaeb',
+    borderRadius: 16,
+    padding: 20,
+    height: 140,
+    width: 140,
+    marginLeft: 13,
+
+      
+  },
+  categoriaTexto: {
+    fontSize: 14,
+    color: '#666',
+    marginBottom: 4,
+  },
+  categoriaValor: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  fixoTexto: {
+    fontSize: 14,
+    color: '#333',
+  },
+  flexivelTexto: {
+    fontSize: 14,
+    color: '#333',
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 24,
+    marginBottom: 16,
+    marginHorizontal: 16,
+    paddingHorizontal: 8,
+  },
+  footerText: {
+    fontSize: 14,
+    color: '#999',
+  },
+});
+
+export default estilosCaju;
